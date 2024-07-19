d8d789765e0eca5c12f581f2010e53c70fcf6365
Sharath Gowda <137074541+SharathxD@users.noreply.github.com>
2024-07-19 18:07:55 +0530

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+
+st.set_page_config(page_title="ChatBot")
+st.header("CHATBOT")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+input=st.text_input("Input:",key="input")
+submit=st.button("Ask the Question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("Response")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot",chunk.text))
+
+
+#UNCOMMENT THE BELOW SECTION TO HAVE THE HISTORY 
+#st.subheader("The chat history is ")
+#for role,text in st.session_state['chat_history']:
+    #st.write(f"{role}:{text}")
